100d918a77598c6f8a3ba334106e348bcc10a36f
OlfaBabai <61467506+OlfaBabai@users.noreply.github.com>
2020-06-09 14:09:42 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NewTicketComponent } from './home/new-ticket/new-ticket.component';
+import { ListTicketComponent } from './home/list-ticket/list-ticket.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home', component : HomeComponent},
+  {path : '', component : LoginComponent},
+  {path : 'newTicket', component : NewTicketComponent},
+  {path : 'listTicket', component : ListTicketComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NewTicketComponent } from './home/new-ticket/new-ticket.component';
+import { ListTicketComponent } from './home/list-ticket/list-ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NewTicketComponent,
+    ListTicketComponent
   ],
   imports: [
     BrowserModule,
